ffcdd8011187f2c1a8d945f379b87a6ae401738b
김건우 <seonghyeonbag@seonghyeon-ui-iMac.local>
2014-02-27 17:47:46 +0900

    Initial Commit

---
+++ TestOpenCV/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  TestOpenCV
+//
+//  Created by 건우 김 on 14. 2. 27..
+//  Copyright (c) 2014년 zero6589@naver.com. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
